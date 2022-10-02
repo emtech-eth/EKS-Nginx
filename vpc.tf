be9859dbb48b9091db498fec277f65bfeec84337
emtech-eth <cloud_user@4488f624a8f94d5595ef508d371702231c.mylabserver.com>
2022-10-02 00:16:28 +0000

    vpc-test

--- vpc.tf
+++ vpc.tf
@@ -18,7 +18,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "testing-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -30,7 +30,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.66.0"
 
-  name                 = "education-vpc"
+  name                 = "testing-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
